255239c99c31f9e182422fa1c306a27f59c1af70
eddiespag-hetti <eddie_p89@hotmail.com>
2024-02-07 16:07:32 +1100

    Created two new files - shapes.js, shapes.test.js
    Added a user prompt array to index.js file

--- Develop/index.js
+++ Develop/index.js
@@ -7,8 +7,34 @@ const { writeFile } = require('fs').promises;
 const promptUser = () => {
     return inquirer.prompt([
         {
-            
-        }
-    ])
+type: 'input',
+name: 'text',
+message: 'What do you want the text to be? ( Up to 3 characters )'
+        },
+        {
+            type: 'input',
+            name: 'text-color',
+            message: 'What color do you want the text to be?'
+        },
+        {
+            type: 'list',
+            name: 'shape',
+            message: 'What shape would you like your logo to have?',
+choices: [
+    'circle', 'triangle', 'square'
+]},
+{
+    type: 'input',
+    name: 'shape-color',
+    message: 'Which color would you like the background to be?'
+},
+])
 }
 
+
+// Funtion to initialize
+const init = () => {
+    promptUser()
+
+    .then((answers) => writeFile('index.html', ))
+}
