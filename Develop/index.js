8f321a7abdf0e46d7f374702a4491469f2d56d8d
eddiespag-hetti <eddie_p89@hotmail.com>
2024-02-07 15:25:48 +1100

    Created index.js file and installed 'inquirer' with npm

---
+++ Develop/index.js
@@ -0,0 +1,14 @@
+// Loading required libraries 
+const inquirer = require('inquirer');
+const { writeFile } = require('fs').promises;
+
+// Array of questions/options
+
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            
+        }
+    ])
+}
+
